46d232d33beb3247ed9a79980d1a140770a50893
calllivecn <c-all@qq.com>
2019-04-02 18:05:18 +0800

    修改：slow_query_top10.sql

--- slow_query_top10.sql
+++ slow_query_top10.sql
@@ -1,4 +1,12 @@
 
 use mysql;
 
-select avg(query_time) as avg_query_time, max(query_time) as max_time,min(query_time) as min_time,count(sql_text) as query_count,sql_text,db from slow_log group by sql_text,db order by avg_query_time desc limit 10;
+select	avg(query_time) as avg_query_time,
+		max(query_time) as max_time,
+		min(query_time) as min_time,
+		count(sql_text) as query_count,
+		sql_text,db 
+		from slow_log 
+		group by sql_text,db 
+		order by avg_query_time 
+		desc limit 10;
